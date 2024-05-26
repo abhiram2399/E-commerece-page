9f9553cbf824de4033f3f23add2f19da8fe6fcfe
abhiram2399 <abhiram.dwaram@gmail.com>
2024-05-26 17:23:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta');
+    ctaButton.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
